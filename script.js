ef765f24232479ea5dae0ae3496b46c26c4f5e45
CodeDude2009 <123761839+CodeDude2009@users.noreply.github.com>
2024-12-01 09:59:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('nuevo-meme').addEventListener('click', function() {
     var imagen = document.getElementById('imagen');
     var imagenes = [];
     for (var i = 1; i <= 50; i++) {
-        imagenes.push('meme ' + i + '.png');
+        imagenes.push('memes/meme (' + i + ').png');
     }
     var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
     imagen.src = imagenes[indiceAleatorio];
